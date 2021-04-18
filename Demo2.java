5dc6ff3c99257ed8d8752db7856a6cfc79dbbcac
dhpme <dheeraj.pant@gmail.com>
2021-04-18 16:09:25 +0000

    Some new changes

--- Demo2.java
+++ Demo2.java
@@ -3,5 +3,7 @@ class Demo2
 public static void main (String args[])
 {
 	System.out.println("Java Demo Class 2");
+	System.out.println("Java Demo Class 2 line 2");
+	System.out.println("Java Demo Class 2 line 3");
 }
 }
